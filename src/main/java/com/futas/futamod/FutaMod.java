43323511633e8a701ef5176d18c8f0e2305c9862
TomasDiLeo <tomasjdle@gmail.com>
2024-09-28 12:25:31 -0300

    Created the famous Sorete Item

--- src/main/java/com/futas/futamod/FutaMod.java
+++ src/main/java/com/futas/futamod/FutaMod.java
@@ -1,5 +1,6 @@
 package com.futas.futamod;
 
+import com.futas.futamod.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -22,6 +23,8 @@ public class FutaMod
     public FutaMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
--- src/main/java/com/futas/futamod/item/ModItems.java
+++ src/main/java/com/futas/futamod/item/ModItems.java
@@ -1,14 +1,19 @@
 package com.futas.futamod.item;
 
 import com.futas.futamod.FutaMod;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, FutaMod.MOD_ID);
+
+    public static final RegistryObject<Item> SORETE = ITEMS.register("sorete",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus)
     {
